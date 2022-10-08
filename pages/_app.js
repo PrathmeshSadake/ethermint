ee4c0aa7956878112644066f4b4f771052775b46
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-10-08 11:48:32 +0530

    created pages for file based routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+
+import { Footer, Navbar } from "../components";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class'>
+      <div className='dark:bg-nft-dark bg-white'>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
