f10a79adda85f5f722388ef51a87699493520ee6
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-05-18 00:48:37 +0530

    Removed old files

--- components/index.js
+++
@@ -1,5 +0,0 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Button from "./Button";
-
-export { Navbar, Footer, Button };
--- pages/listed-nfts.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListedNFTs = () => {
-  return <div>ListedNFTs</div>;
-};
-
-export default ListedNFTs;
